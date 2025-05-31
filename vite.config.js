f5a8391343e7fa0256d1c3e9c2b3273a9cdaed88
chandanhm1999 <chandanchanduhm4@gmail.com>
2025-05-31 16:51:15 +0530

    deply error

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'build', // Change this to 'build' to match Vercel's expectation
-  }
-})
+  // Removed 'outDir' so Vite defaults to 'dist'
+});
