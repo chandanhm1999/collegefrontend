80c9c9e5685dad99049a43a60874ee3adc070ea1
chandanhm1999 <chandanchanduhm4@gmail.com>
2025-05-31 21:04:05 +0530

    endpoint

--- src/components/Auth/Register.jsx
+++ src/components/Auth/Register.jsx
@@ -27,7 +27,7 @@ const Register = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "https://collegebackend.vercel.app/api/v1/user/register",
+        "https://newcollegebackend.vercel.app/api/user/register",
         { name, phone, email, role, password },
         {
           headers: { "Content-Type": "application/json" },
