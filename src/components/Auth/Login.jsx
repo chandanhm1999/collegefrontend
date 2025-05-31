a4cf76ef90ee176037daa3f4139dfb271f05755f
chandanhm1999 <chandanchanduhm4@gmail.com>
2025-05-31 16:39:23 +0530

    first commit

---
+++ src/components/Auth/Login.jsx
@@ -0,0 +1,121 @@
+import React, { useContext, useState } from "react";
+import { MdOutlineMailOutline } from "react-icons/md";
+import { RiLock2Fill } from "react-icons/ri";
+import { Link, Navigate } from "react-router-dom";
+import { FaRegUser } from "react-icons/fa";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { Context } from "../../main";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [role, setRole] = useState("");
+
+  const { isAuthorized, setIsAuthorized, setUser } = useContext(Context);
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post(
+        "https://collegebackend.vercel.app/api/v1/user/login",
+        { email, password, role },
+        {
+          headers: { "Content-Type": "application/json" },
+          withCredentials: true,
+        }
+      );
+      toast.success(data.message);
+      setEmail("");
+      setUser(data.user);
+      setPassword("");
+      setRole("");
+      setIsAuthorized(true);
+    } catch (error) {
+      toast.error(error.response?.data?.message || "Login Failed");
+    }
+  };
+
+  if (isAuthorized) {
+    return <Navigate to={"/"} />;
+  }
+
+  return (
+    <>
+      <section className="authPage">
+        <div className="container">
+          <div className="header">
+            <img src="/placement.png" alt="logo" />
+            <h3>Login to your account</h3>
+          </div>
+          <form>
+            <div className="inputTag">
+              <label>Login As</label>
+              <div>
+                <select value={role} onChange={(e) => setRole(e.target.value)}>
+                  <option value="">Select Role</option>
+                  <option value="Employer">Admin</option>
+                  <option value="Job Seeker">Student</option>
+                  <option value="Recruiters">Recruiters</option>
+                </select>
+                <FaRegUser />
+              </div>
+            </div>
+            <div className="inputTag">
+              <label>Email Address</label>
+              <div>
+                <input
+                  type="email"
+                  placeholder="zk@gmail.com"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <MdOutlineMailOutline />
+              </div>
+            </div>
+            <div className="inputTag">
+              <label>Password</label>
+              <div style={{ position: "relative", width: "100%" }}>
+                <input
+                  type={showPassword ? "text" : "password"}
+                  placeholder="Your Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  style={{ paddingRight: "40px" }}
+                />
+
+                {/* Eye toggle button */}
+                <span
+                  onClick={() => setShowPassword((prev) => !prev)}
+                  style={{
+                    position: "absolute",
+                    right: "10px",
+                    top: "50%",
+                    transform: "translateY(-50%)",
+                    cursor: "pointer",
+                    color: "#2d5649",
+                    fontSize: "1.2rem",
+                    zIndex: 1,
+                  }}
+                >
+                  {showPassword ? <FaEyeSlash /> : <FaEye />}
+                </span>
+              </div>
+            </div>
+            <button type="submit" onClick={handleLogin}>
+              Login
+            </button>
+            <Link to={"/register"}>Register Now</Link>
+          </form>
+        </div>
+        <div className="banner">
+          <img src="/login.png" alt="login" />
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Login;
